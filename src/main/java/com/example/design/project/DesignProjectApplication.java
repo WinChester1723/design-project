02042bceef4027cc39754b63883e32d02eea82e2
Pasha <qordon23@gmail.com>
2022-08-18 19:34:14 +0400

    Initial commit

---
+++ src/main/java/com/example/design/project/DesignProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.design.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignProjectApplication.class, args);
+	}
+
+}
