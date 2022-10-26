410b42418e7f9abcb0888018009e08bae39aca29
Pasha <qordon23@gmail.com>
2022-10-26 12:51:38 +0400

    fixed config bug, but used depricated class,becase mvcsec crashed this prog

--- src/main/java/com/example/design/project/dao/repository/RoleRepository.java
+++ src/main/java/com/example/design/project/dao/repository/RoleRepository.java
@@ -3,7 +3,9 @@ package com.example.design.project.dao.repository;
 import com.example.design.project.dao.entity.RoleEntity;
 import com.example.design.project.model.enums.RoleEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<RoleEntity, Integer> {
     RoleEntity findByRoleName(RoleEnum roleEnum);
 }
